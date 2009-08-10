9100e1aaefe5d9a69670dc0e395a42a3deda2c94
MJ Richardson <michael.james.richardson@gmail.com>
2009-08-11 00:01:12 +1000

    first commit

---
+++ Viking/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Company.Viking
+{
+    static class GuidList
+    {
+        public const string guidVikingPkgString = "000a554a-ca9a-44b9-ae2b-de75ff9d1e03";
+        public const string guidVikingCmdSetString = "d40f99c9-f139-4b58-af75-15b14ca14185";
+
+        public static readonly Guid guidVikingCmdSet = new Guid(guidVikingCmdSetString);
+    };
+}
